80a3e7a78c7023daf23d1d463c5250490deee2ea
Jan Taras <jan.taras@sage.com>
2019-07-10 16:32:42 +0200

    [sql_tracker] Added sorting by difference in each category

--- comparison.rb
+++ comparison.rb
@@ -2,61 +2,86 @@
 
 require 'json'
 
-if ARGV.size != 2
-	print_usage
-	raise StandardError, 'You need to provide 2 files in order to compare them' 
-end
+class Comparator
+	LOG_NAME = "comparison.log"
 
-begin
-	f0 = File.read(ARGV[0])
-	f1 = File.read(ARGV[1])
-rescue StandardError => e
-	puts e.message
-	exit(1)
-end
-LOG_NAME = "comparison.log"
+	attr_accessor :master, :feature, :f0, :f1
 
-begin
-	master = JSON.load(f0)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
-	feature = JSON.load(f1)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
-rescue JSON::ParserError
-	error 'One of the files provided is not a correctly formatted JSON file'
-end
-
-File.open(LOG_NAME, "w") do |io| 
-	io.write("SQL Query Count Decreases between #{ARGV[0]} -> #{ARGV[1]}\n")
-	io.write("##########################################################\n")
-	(master.keys | feature.keys).each do |key|
-		next unless master[key] && feature[key] && master[key] > feature[key]
-		io.write("Difference for #{key}:\n")
-		io.write("Count drop: #{master[key]} -> #{feature[key]}\n")
-	end
-	io.write("SQL Query Count Increases between #{ARGV[0]} -> #{ARGV[1]}\n")
-	io.write("##########################################################\n")
-	(master.keys | feature.keys).each do |key|
-		next unless master[key] && feature[key] && master[key] < feature[key]
-		io.write("Difference for #{key}:\n")
-		io.write("Count difference: #{master[key]} -> #{feature[key]}\n")
+	def initialize
+		load_files
 	end
 
-	io.write("SQL New Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
-	io.write("##########################################################\n")
-	(feature.keys - master.keys).each do |key|
-		next unless feature[key]
-		io.write("Difference for #{key}:\n")
-		io.write("Count difference: 0 -> #{feature[key]}\n")
+	def generate_report
+		File.open(LOG_NAME, "w") do |io| 
+			io.write("SQL Query Count Decreases between #{ARGV[0]} -> #{ARGV[1]}\n")
+			io.write("##########################################################\n")
+			results = process_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] > feature[key] }
+			results.each do |key, counts|
+				io.write("Difference for #{key}:\n")
+				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+			end
+			io.write("SQL Query Count Increases between #{ARGV[0]} -> #{ARGV[1]}\n")
+			io.write("##########################################################\n")
+			results = process_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] < feature[key] }
+			results.each do |key, counts|
+				io.write("Difference for #{key}:\n")
+				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+			end
+		
+			io.write("SQL New Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
+			io.write("##########################################################\n")
+			results = process_triplets(feature.keys - master.keys) { |key| feature[key] }
+			results.each do |key, counts|
+				io.write("Difference for #{key}:\n")
+				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+			end
+		
+			io.write("SQL Gone Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
+			io.write("##########################################################\n")
+			results = process_triplets(master.keys - feature.keys) { |key| master[key] }
+			results.each do |key, counts|
+				io.write("Difference for #{key}:\n")
+				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+			end
+		end
 	end
 
-	io.write("SQL Gone Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
-	io.write("##########################################################\n")
-	(master.keys - feature.keys).each do |key|
-		next unless master[key]
-		io.write("Difference for #{key}:\n")
-		io.write("Count difference: #{master[key]} -> 0\n")
+	private
+
+	def process_triplets(collection)
+		triplets = collection.select { |key| yield(key) }.map { |key| [key, [master[key] || 0, feature[key] || 0]] }
+		triplets.sort_by {|t| (t[1][0] - t[1][1]).abs }.reverse.to_h
+	end
+	
+	def print_usage
+		puts "Usage: ruby comparison.rb first_file.json second_file.json"
+	end
+
+	def load_files
+		begin
+			@f0 = File.read(ARGV[0])
+			@f1 = File.read(ARGV[1])
+		rescue StandardError => e
+			puts e.message
+			exit(1)
+		end
+
+		begin
+			@master = JSON.load(f0)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
+			@feature = JSON.load(f1)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
+		rescue JSON::ParserError
+			error 'One of the files provided is not a correctly formatted JSON file'
+		end
+	end
+
+	def ensure_params_passed
+		if ARGV.size != 2
+			print_usage
+			raise StandardError, 'You need to provide 2 files in order to compare them' 
+		end
 	end
 end
 
-def print_usage
-	puts "Usage: ruby comparison.rb first_file.json second_file.json"
-end
+
+Comparator.new.generate_report
 
