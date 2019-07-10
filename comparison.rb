920cf47e58a7adb896e47c728110bb230d85131a
Jan Taras <jan.taras@sage.com>
2019-07-10 17:06:33 +0200

    [sql_reporter] Sorting by abs difference and number of queries

--- comparison.rb
+++ comparison.rb
@@ -5,52 +5,47 @@ require 'json'
 class Comparator
 	LOG_NAME = "comparison.log"
 
-	attr_accessor :master, :feature, :f0, :f1
+	attr_accessor :master, :feature, :f0, :f1, :io
 
 	def initialize
 		load_files
 	end
 
 	def generate_report
-		File.open(LOG_NAME, "w") do |io| 
-			io.write("SQL Query Count Decreases between #{ARGV[0]} -> #{ARGV[1]}\n")
-			io.write("##########################################################\n")
-			results = process_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] > feature[key] }
-			results.each do |key, counts|
-				io.write("Difference for #{key}:\n")
-				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
-			end
-			io.write("SQL Query Count Increases between #{ARGV[0]} -> #{ARGV[1]}\n")
-			io.write("##########################################################\n")
-			results = process_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] < feature[key] }
-			results.each do |key, counts|
-				io.write("Difference for #{key}:\n")
-				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
-			end
-		
-			io.write("SQL New Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
-			io.write("##########################################################\n")
-			results = process_triplets(feature.keys - master.keys) { |key| feature[key] }
-			results.each do |key, counts|
-				io.write("Difference for #{key}:\n")
-				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
-			end
-		
-			io.write("SQL Gone Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
-			io.write("##########################################################\n")
-			results = process_triplets(master.keys - feature.keys) { |key| master[key] }
-			results.each do |key, counts|
-				io.write("Difference for #{key}:\n")
-				io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
-			end
-		end
+		@io = File.open(LOG_NAME, "w")
+
+		io.write("SQL Query Count Decreases between #{ARGV[0]} -> #{ARGV[1]}\n")
+		io.write("##########################################################\n")
+		summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] > feature[key] }
+
+		io.write("SQL Query Count Increases between #{ARGV[0]} -> #{ARGV[1]}\n")
+		io.write("##########################################################\n")
+		summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] < feature[key] }
+
+		io.write("SQL New Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
+		io.write("##########################################################\n")
+		summary_for_selected_triplets(feature.keys - master.keys) { |key| feature[key] }
+
+		io.write("SQL Gone Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
+		io.write("##########################################################\n")
+		summary_for_selected_triplets(master.keys - feature.keys) { |key| master[key] }
+
+		io.close()
 	end
 
 	private
 
+	
+	def summary_for_selected_triplets(collection, &block)
+		process_triplets(collection, &block).each do |key, counts|
+			io.write("Difference for #{key}:\n")
+			io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+		end
+	end
+
 	def process_triplets(collection)
 		triplets = collection.select { |key| yield(key) }.map { |key| [key, [master[key] || 0, feature[key] || 0]] }
-		triplets.sort_by {|t| (t[1][0] - t[1][1]).abs }.reverse.to_h
+		triplets.sort_by {|t| (t[1][0] - t[1][1]).abs + t[1][0]* 0.000001 }.reverse.to_h
 	end
 	
 	def print_usage
