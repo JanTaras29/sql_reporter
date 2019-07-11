96c6ce6d463a99a84bcb10515f732ec3c80d8fee
Jan Taras <jan.taras@sage.com>
2019-07-11 11:19:13 +0200

    [sql_tracker] Introduced measurement of time and summary

--- comparison.rb
+++ comparison.rb
@@ -1,6 +1,30 @@
 #! usr/env/ruby
 
 require 'json'
+# Data struct for keeping the query data
+Query = Struct.new(:sql, :count, :duration) do
+	def duration_formatted
+		duration&.round(2)
+	end
+end
+
+Totals = Struct.new(:query_diff, :duration_diff) do
+	def queries_msg
+		"#{query_diff > 0 ? 'Queries spawned' : 'Queries killed' }: #{query_diff.abs}\n" 
+	end
+
+	def duration_msg
+		"#{duration_diff > 0 ? 'Duration gain' : 'Duration decrease' }[ms]: #{duration_diff.abs.round(2)}\n"
+	end
+
+	def summary
+		"##############\n" + queries_msg + duration_msg + "##############\n"
+	end
+
+	def +(total)
+		self.class.new(query_diff + total.query_diff, duration_diff + total.duration_diff)
+	end
+end
 
 class Comparator
 	LOG_NAME = "comparison.log"
@@ -13,39 +37,64 @@ class Comparator
 
 	def generate_report
 		@io = File.open(LOG_NAME, "w")
+		totals = []
 
 		io.write("SQL Query Count Decreases between #{ARGV[0]} -> #{ARGV[1]}\n")
 		io.write("##########################################################\n")
-		summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] > feature[key] }
+		totals << summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key].count > feature[key].count }
 
 		io.write("SQL Query Count Increases between #{ARGV[0]} -> #{ARGV[1]}\n")
 		io.write("##########################################################\n")
-		summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key] < feature[key] }
+		totals << summary_for_selected_triplets(master.keys | feature.keys) { |key| master[key] && feature[key] && master[key].count < feature[key].count }
 
 		io.write("SQL New Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
 		io.write("##########################################################\n")
-		summary_for_selected_triplets(feature.keys - master.keys) { |key| feature[key] }
+		totals << summary_for_selected_triplets(feature.keys - master.keys) { |key| feature[key] }
 
 		io.write("SQL Gone Queries between #{ARGV[0]} -> #{ARGV[1]}\n")
 		io.write("##########################################################\n")
-		summary_for_selected_triplets(master.keys - feature.keys) { |key| master[key] }
+		totals << summary_for_selected_triplets(master.keys - feature.keys) { |key| master[key] }
 
-		io.close()
+		io.write("################## SUMMARY #####################\n")
+		summary = totals.reduce(Totals.new(0,0)) {|acc, t| acc + t}.summary
+		io.write(summary)
+		io.close
 	end
 
 	private
 
-	
 	def summary_for_selected_triplets(collection, &block)
-		process_triplets(collection, &block).each do |key, counts|
+		duration_diff = 0
+		query_diff = 0
+		process_triplets(collection, &block).each do |key, queries|
 			io.write("Difference for #{key}:\n")
-			io.write("Count difference: #{counts[0]} -> #{counts[1]}\n")
+			io.write("Count difference: #{queries['master'].count} -> #{queries['feature'].count}\n")
+			io.write("Duration difference [ms]: #{queries['master'].duration_formatted} -> #{queries['feature'].duration_formatted}\n")
+			query_diff += queries['feature'].count - queries['master'].count
+			duration_diff += queries['feature'].duration_formatted - queries['master'].duration_formatted
 		end
+		totals  = Totals.new(query_diff, duration_diff)
+		io.write(totals.summary)
+		totals
 	end
 
 	def process_triplets(collection)
-		triplets = collection.select { |key| yield(key) }.map { |key| [key, [master[key] || 0, feature[key] || 0]] }
-		triplets.sort_by {|t| (t[1][0] - t[1][1]).abs + t[1][0]* 0.000001 }.reverse.to_h
+		prefiltered_collection = collection.select { |key| yield(key) }
+		triplets = prefiltered_collection.map do |key| 
+			[key, construct_query_comparison_hash(master[key], feature[key], key)]
+		end
+		triplets.sort_by {|t| (t[1]['master'].count - t[1]['feature'].count).abs + t[1]['master'].count* 0.000001 }.reverse.to_h
+	end
+
+	def construct_query_comparison_hash(master, feature, key)
+		hsh = {}
+		hsh['master'] = master || null_query(key)
+		hsh['feature'] = feature || null_query(key)
+		hsh
+	end
+
+	def duration_summary(diff)
+		"#{diff > 0 ? 'Duration gain' : 'Duration decrease' }[ms]: #{diff.abs}\n"
 	end
 	
 	def print_usage
@@ -62,8 +111,8 @@ class Comparator
 		end
 
 		begin
-			@master = JSON.load(f0)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
-			@feature = JSON.load(f1)['data'].values.map { |v| [v['sql'], v['count']] }.to_h
+			@master = JSON.load(f0)['data'].values.map { |v| [v['sql'], Query.new(v['sql'], v['count'], v['duration'])] }.to_h
+			@feature = JSON.load(f1)['data'].values.map { |v| [v['sql'], Query.new(v['sql'], v['count'], v['duration'])] }.to_h
 		rescue JSON::ParserError
 			error 'One of the files provided is not a correctly formatted JSON file'
 		end
@@ -74,6 +123,10 @@ class Comparator
 			print_usage
 			raise StandardError, 'You need to provide 2 files in order to compare them' 
 		end
+	end
+
+	def null_query(query_name)
+		Query.new(query_name, 0, 0)
 	end
 end
 
